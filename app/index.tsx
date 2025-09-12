eb769f4089d297da766d94244e987ddbeb6cbf10
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-09-12 20:15:19 +0500

    Initialize pnpm workspace and add Tailwind CSS configuration
    - Added pnpm-workspace.yaml to configure node linker as hoisted.
    - Created tailwind.config.js with basic setup for Tailwind CSS and NativeWind preset.
    - Added tsconfig.json extending Expo's base configuration with strict type checking and path aliases.

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View className="">
+			<Text className="text-5xl text-red-600">Hello</Text>
+		</View>
+	);
+}
