dee042ee136510148834f611b6c68b0dc92df81f
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-09-13 22:44:30 +0500

    Update app name and icon, and remove unused Android assets

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,25 +25,18 @@ const _Layout = () => {
 		<Tabs
 			screenOptions={{
 				tabBarShowLabel: false,
-				tabBarItemStyle: {
-					width: "100%",
-					height: "100%",
-					justifyContent: "center",
-					alignItems: "center",
-				},
 				tabBarStyle: {
 					backgroundColor: "#0F0D23",
 					marginHorizontal: 20,
 					marginBottom: 36,
-					paddingHorizontal: 9.5,
-					height: 55,
+					paddingHorizontal: 4,
+					height: 50,
 					position: "absolute",
 					overflow: "hidden",
 					borderRadius: 50,
-					borderColor: "0F0D23",
 				},
-				tabBarIconStyle: {
-					marginTop: 9,
+				tabBarItemStyle: {
+					marginTop: 6,
 				},
 			}}
 		>
